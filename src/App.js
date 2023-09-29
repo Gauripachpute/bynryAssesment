128129f8324ef434ea10e5d2f64c92c1d9ac6bb1
ShubhamKH08 <hagawaneshubham908@gmail.com>
2023-09-29 13:54:56 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-
-import { Route, Link, BrowserRouter as Router, Routes } from 'react-router-dom';
-import './App.css';
-import LoginForm from './Auth/Login';
-import SignInForm from './Auth/SignUp';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import ProfileList from "./components/ProfileList";
+import ProfileMap from "./components/ProfileMap";
+import profilesData from "./data/profiles.json";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-
-<Router>
-     {/* <LoginForm></LoginForm> */}
-     {/* <SignInForm></SignInForm> */}
-
-     <Routes>
-              <Route exact path='/' element={<LoginForm/>}></Route>
-              <Route exact path='/signUp' element={<SignInForm/>}></Route>
-              
-            </Routes>
-              </Router>
+    <div className="app">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={() => <ProfileList profiles={profilesData} />} />
+          {/* Define a route for ProfileMap that captures route parameters */}
+          <Route
+            path="/map/:profileId/:latitude/:longitude"
+            component={ProfileMap}
+          />
+        </Switch>
+      </Router>
     </div>
   );
 }
